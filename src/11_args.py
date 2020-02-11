c38a1091e4f7416fab3052189a95fff470303afe
vdtyson <versilistyson@gmail.com>
2020-02-11 18:08:03 -0500

    12

--- src/11_args.py
+++ src/11_args.py
@@ -5,24 +5,35 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(int1: int, int2: int):
+    return int1 + int2
+
 
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(intList: []):
+    x = int
+    for int in intList:
+        x += int
+    return x
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(a))  # Should print 22
+
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,9 +41,17 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(intList: list()):
+    if len(intList) == 1:
+        return 9
+    elif len(intList) == 2:
+        return 3
+    else:
+        return 0
+
 
 print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+print(f3(8))  # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -48,6 +67,11 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
+def f4(*positional, **keywords):
+    print("Positional:", positional)
+    print("Keywords:", keywords)
+
+
 f4(a=12, b=30)
 
 # Should print
