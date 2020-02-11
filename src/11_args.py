c9cb24aa7fe606463cdd9a223b7b9a7645405f50
vdtyson <versilistyson@gmail.com>
2020-02-11 18:29:57 -0500

    12

--- src/11_args.py
+++ src/11_args.py
@@ -5,53 +5,39 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-def f1(int1: int, int2: int):
-    return int1 + int2
-
-
+def f1(x,y):
+    return x+y
 print(f1(1, 2))
 
-
 # Write a function f2 that takes any number of integer arguments and prints the
-# sum.
-# Note: Google for "python arbitrary arguments" and look for "*args"
+# sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-def f2(intList: []):
-    x = int
-    for int in intList:
-        x += int
-    return x
+def f2(*args):
+    result = 0
+    for i in args:
+        result += i
+    return result
 
-
-print(f2(1))  # Should print 1
-print(f2(1, 3))  # Should print 4
-print(f2(1, 4, -12))  # Should print -7
+print(f2(1))                    # Should print 1
+print(f2(1, 3))                 # Should print 4
+print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# How do you have to modify the f2 call below to make this work?
-print(f2(a))  # Should print 22
-
+# What thing do you have to add to make this work?
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
-# arguments.
-# Note: Google "python default arguments" for a hint.
+# arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-def f3(intList: list()):
-    if len(intList) == 1:
-        return 9
-    elif len(intList) == 2:
-        return 3
-    else:
-        return 0
-
-
+def f3(x,y=1):
+    return x + y
 print(f3(1, 2))  # Should print 3
-print(f3(8))  # Should print 9
+print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -60,18 +46,15 @@ print(f3(8))  # Should print 9
 # key: foo, value: bar
 # key: baz, value: 12
 #
-# Note: Google "python keyword arguments".
+# Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**kwargs):
+    for i in kwargs:
+        print(f'key: {i}, value: {kwargs[i]}')
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-def f4(*positional, **keywords):
-    print("Positional:", positional)
-    print("Keywords:", keywords)
-
-
 f4(a=12, b=30)
 
 # Should print
@@ -85,5 +68,5 @@ d = {
     "hp": 3
 }
 
-# How do you have to modify the f4 call below to make this work?
+# What thing do you have to add to make this work?
 f4(d)
